cfcf780fb9cc34f1af9f4147c7749ed16ccb7f90
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-06-22 19:53:05 +0530

    Update filters.py

--- maintain/filters.py
+++ maintain/filters.py
@@ -1,7 +1,5 @@
 from typing import Union, List
-
 from pyrogram import filters
-
 from config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
@@ -10,3 +8,4 @@ other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filt
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
+
